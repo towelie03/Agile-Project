3f574e06f3ddb6e611d2474f4c8ba5eef25bb84e
ligmanoots <dbalsara2003@gmail.com>
2022-04-30 16:34:26 -0700

    added a route for the index.html page. now you can test by running npm start

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const express = require('express');
 const bp = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8080;
-const db = mongoose.connect("mongodb://localhost/");
+//const db = mongoose.connect("mongodb://localhost/");
 const router = express.Router();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bp.urlencoded({ extended: true }));
+app.use(bp.json());
 
 
 app.use("/", router);
 
 app.get("/", (req, res) => {
-	res.send(__dirname + "./templates/index.html");
+	res.sendFile(__dirname + "/templates/index.html");
 });
 
 

97a1b618c4fb183179526ef734b6c9480dc3d02a
ligmanoots <dbalsara2003@gmail.com>
2022-04-30 17:35:01 -0700

    added the routes for the about and story pages

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mongo = require('mongodb');
 const express = require('express');
 const bp = require('body-parser');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 8080;
 //const db = mongoose.connect("mongodb://localhost/");
@@ -12,11 +14,22 @@ app.use(bp.json());
 
 app.use("/", router);
 
-app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/templates/index.html");
+app.get("/index.html", (req, res) => {
+	res.sendFile(path.join(__dirname + "/templates/index.html"));
 });
+
+app.get("/about.html", (req, res) => {
+	res.sendFile(path.join(__dirname + "/templates/about.html"));
+});
+
+app.get("/story.html", (req, res) => {
+	res.sendFile(path.join(__dirname + "/templates/story.html"));
+});
+
+
 
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
+    console.log('press CTRL + C to quit');
 });

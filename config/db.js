a912682503dbe1a05023b395c2c629b2a539925a
Ayman <aymanm979@gmail.com>
2022-05-03 10:44:16 -0700

    Added db config

--- config/db.js
+++ config/db.js
@@ -1,8 +1,17 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:P@ssw0rd@agile.ja8u2.mongodb.net/Agile?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const mongoose = require('mongoose');
+
+const url = "mongodb+srv://admin:P@ssw0rd@agile.ja8u2.mongodb.net/Agile?retryWrites=true&w=majority";
+
+const connectionParams={
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true 
+};
+
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })

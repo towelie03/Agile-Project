16bbc139a813493d4a7636eac466c296d887d163
Daryush Balsara <56945036+ligmanoots@users.noreply.github.com>
2022-05-03 05:04:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,109 +3,32 @@ const express = require('express');
 const bp = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8080;
-const db = mongoose.connect("mongodb://localhost/courseAPI");
 const router = express.Router();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use("/", router);
 
-router
-    .route("/apis")
-    .post((req, res) => {
-        const course = new Course(req.body);
-        course.save();
-        return res.status(201).json(course);
-    })
-    .get((req, res) => {
-        const course = new Course(req.body);
-        const query = {};
-        if (req.query.genre) {
-            query.InstName = req.query.InstName;
-        }
-        Course.find(query, (err, courses) => {
-            if (err) {
-                return res.send(err);
-            }
-            return res.json(courses);
-        })
-    })
-    .delete((req, res) => {
-        const course = new Course(req.body);
-        const query = {};
-        Course.find(query, (err, courses) => {
-            if (err) {
-                return res.send(err);
-            }
-            for (i of courses) { i.remove() };
-            return res.json(courses);
-        })
-    });
-
-courseRouter.use("/api/:id", (req, res, next) => {
-    Course.findById(req.params.id, (err, course) => {
-        if (err) {
-            return res.send(err);
-        }
-        if (course) {
-
-            req.course = course;
-            return next();
-        }
-        return res.sendStatus(404);
-    });
+app.get("/test", (req, res) => {
+	res.json({ msg: "This is a test for the mongo auth" });
 });
 
-courseRouter
-    .route("/api/:id")
-    .post((req, res) => {
-        const course = new Course(req.body);
-        course.save();
-        return res.status(201).json(course);
-    })
-
-    .delete((req, res) => {
-        req.course.remove((err) => {
-            if (err) {
-                return res.send(err);
-            }
-            return res.json(req.course);
-        });
-    })
-    .patch((req, res) => {
-        const { course } = req;
-        course.title = req.body.Dateandtime;
-        course.author = req.body.course;
-        course.genre = req.body.Location;
-        course.read = req.body.InstName;
-        course.save();
-        return res.json(course);
-    })
-
-    .put((req, res) => {
-        const { course } = req;
-        course.title = req.body.Dateandtime;
-        course.author = req.body.course;
-        course.genre = req.body.Location;
-        course.read = req.body.InstName;
-        course.save();
-        return res.json(course);
-    })
-    .get((req, res) => {
-
-        Course.findById(req.params.id, (err, course) => {
-            if (err) {
-                return res.send(err);
-            }
-            return res.json(course);
-        });
-    });
-
-
-app.use("/api", courseRouter);
-
 app.get("/", (req, res) => {
-    const course = new Course(req.body);
-    res.send("Welcome to my API!");
+	res.sendFile(path.join(__dirname + "/templates/index.html"));
+});
+
+app.get("/about", (req, res) => {
+	res.sendFile(path.join(__dirname + "/templates/about.html"));
+});
+
+app.get("/story", (req, res) => {
+	res.sendFile(path.join(__dirname + "/templates/story.html"));
+});
+
+app.get("/login", (req, res) => {
+	res.sendFile(path.join(__dirname + "/templates/login.html"));
+});
+
+app.get("/signup", (req, res) => {
+	res.sendFile(path.join(__dirname + "/templates/signup.html"));
 });
 
 

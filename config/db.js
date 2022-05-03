ec2dfa29fe4d138b05a5ae61cdf6d82a64b5e039
Ayman <aymanm979@gmail.com>
2022-05-03 10:47:04 -0700

    fixed db config

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const url = "mongodb+srv://admin:P@ssw0rd@agile.ja8u2.mongodb.net/Agile?retryWrites=true&w=majority";
@@ -16,13 +15,3 @@ mongoose.connect(url,connectionParams)
     .catch( (err) => {
         console.error(`Error connecting to the database. \n${err}`);
     })
-=======
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:P@ssw0rd@agile.ja8u2.mongodb.net/Agile?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
->>>>>>> 061927ef446283ff0efd398feb388575ef53e276

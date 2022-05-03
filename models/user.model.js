622a7ba7324d572e33256567d27d21e758b4396f
ligmanoots <dbalsara2003@gmail.com>
2022-05-02 22:00:03 -0700

    adding the user model

---
+++ models/user.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    full_name: String,
+    email: String,
+    username: String,
+    password: String,
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role"
+      }
+    ]
+  })
+);
+module.exports = User;
+

add69f300d3ee1063cc0dc390b7e915cb44042cf
ligmanoots <dbalsara2003@gmail.com>
2022-05-03 10:32:28 -0700

    test

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:<P@ssw0rd>@agile.ja8u2.mongodb.net/Agile?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:P@ssw0rd@agile.ja8u2.mongodb.net/Agile?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");

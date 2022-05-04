065a1df52fedd518d4334ab645cf4e440d41dc9b
ligmanoots <dbalsara2003@gmail.com>
2022-05-04 15:52:25 -0700

    need to export the endpoints for unit testing with jest and supertest

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ const bp = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
 const db = require("./config/db")
 const router = express.Router();
 
-
 app.use("/", router);
-
-app.use(cors(corsOptions));
 
 app.use(express.json());
 
@@ -48,7 +44,6 @@ app.get("/timers", (req, res) => {
 	res.sendFile(path.join(__dirname + "/templates/timers.html"))
 })
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}`);
-    console.log('press CTRL + C to quit');
-});
+
+module.exports = app;
+

896d68ef497724dad300cf65ea011ff90774456d
AndrewTake <andrew6041@gmail.com>
2022-05-04 01:34:27 -0700

    nav, toggle

--- templates/src/script.js
+++ templates/src/script.js
@@ -1,57 +1,12 @@
+var check = false
 
-// // .toggle
-// let darkTheme = () => {
-//     let switchDarkThemeButton = document.getElementById("theme");
-//     // switchDarkTheme.classList.toggle("dark-theme");
-//     switchDarkThemeButton.addEventListener('click', () => {
-//         let mainCon = document.querySelector(".main-container");
-
-//         mainCon.classList.toggle("dark-theme");
-//     })
-// }
-
-// darkTheme()
-
-// // const lightTheme = () => {
-// //   let switchLightTheme = document.querySelector(".main-container");
-// //   switchLightTheme.classList.replace("dark-theme", "light-theme");
-// // }
-
-// function darkMode() {
-//     if (isLightModeOn == false) {
-//         darkTheme()
-//         isLightModeOn = true
-//     } else {
-//         lightTheme()
-//         isLightModeOn = false
-//     }
-// }
-
-let check = false
-function createCheck() {
-
-    var clickBox = document.querySelector('.theme-toggle')
-    clickBox.addEventListener('click', () => {
-        const dark = document.querySelector("body")
-        if (check == false) {
-            dark.classList.replace('white', 'black')
-            check = true
-        } else if (check == true) {
-            dark.classList.replace('black', 'white')
-            check = false
-        }
-    })
-    console.log("hello")
+function LDmode() {
+    const dark = document.querySelector(".main-container")
+    if (check == false) {
+        dark.classList.replace('light-theme', 'dark-theme')
+        check = true
+    } else if (check == true) {
+        dark.classList.replace('dark-theme', 'light-theme')
+        check = false
+    }
 }
-
-// const LDmode = () => {
-//     const dark = document.querySelector("body")
-//     if (check == false) {
-//         dark.classList.replace('white', 'black')
-//         check = true
-//     } else if (check == true) {
-//         dark.classList.replace('black', 'white')
-//         check = false
-//     }
-// }
-createCheck()

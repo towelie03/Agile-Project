21b9241fd00e5414e61feb962a360d787ee4653f
ligmanoots <dbalsara2003@gmail.com>
2022-05-04 23:07:22 -0700

    finally i put this in a function

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 import { MongoClient } from "mongodb";
+const data_base = MongoClient;
 
 const pass = encodeURIComponent("P@ssw0rd")
 
 const con_string = `mongodb+srv://admin:${pass}@agile.ja8u2.mongodb.net/tester?retryWrites=true&w=majority`;
 
 
-MongoClient.connect(con_string, function(err, db) {
+data_base.connect(con_string, function(err, db) {
 
     const acc = db.db().admin();
     acc.listDatabases(async function(err, result) {
@@ -18,3 +19,5 @@ MongoClient.connect(con_string, function(err, db) {
     }
     });
   });
+
+export default data_base;

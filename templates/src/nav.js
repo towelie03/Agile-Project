9bb1bb68a38dcf083c552a570ff53d2adf51ae95
Ayman <aymanm979@gmail.com>
2022-05-04 23:06:39 -0700

    fixed nav

--- templates/src/nav.js
+++ templates/src/nav.js
@@ -1,46 +1,41 @@
-import {LitElement, html} from 'lit';
 
-class MyElement extends LitElement {
-  render() {
-    return html`
+ function navs(){
+  const nav_div = document.querySelector("#navbar")
+  nav_div.insertAdjacentHTML("beforebegin",
+  `   <nav>
+  <ul class="nav_link_list">
+  <li class="nav_link">
+  <a href="index.html" class="
+  nav_link_a
+  link__hover-effect
+  ">Home</a>
+</li>
+<li class="nav_link">
+<a href="about.html" class="
+  nav_link_a
+  link__hover-effect
+  ">About</a>
+</li>
+<li class="nav_link">
+<a href="story.html" class="
+  nav_link_a
+  link__hover-effect
+  ">Story</a>
+</li>
+<li class="nav_link">
+<a href="timers.html" class="
+  nav_link_a
+  link__hover-effect
+  ">Timer</a>
+</li>
+<li class="nav_link">
+<a href="login.html" class="
+  nav_link_a
+  link__hover-effect
+  "> Login/Signup</a>
+</li>
 
-      <nav>
-            <ul class="nav_link_list">
-            <li class="nav_link">
-            <a href="index.html" class="
-            nav_link_a
-            link__hover-effect
-            ">Home</a>
-      </li>
-      <li class="nav_link">
-        <a href="about.html" class="
-            nav_link_a
-            link__hover-effect
-            ">About</a>
-      </li>
-      <li class="nav_link">
-        <a href="story.html" class="
-            nav_link_a
-            link__hover-effect
-            ">Story</a>
-      </li>
-      <li class="nav_link">
-        <a href="timers.html" class="
-            nav_link_a
-            link__hover-effect
-            ">Timer</a>
-      </li>
-      <li class="nav_link">
-        <a href="login.html" class="
-            nav_link_a
-            link__hover-effect
-            "> Login/Signup</a>
-      </li>
-
-  </nav>
-
-
-          `;
-  }
+</nav> `
+  )
 }
-customElements.define('nav_bar', navbar);
+navs()

5cbe9db222ed874ed13210d1b5ad32775ce77953
ligmanoots <dbalsara2003@gmail.com>
2022-05-04 19:33:02 -0700

    this is the testfile for the index.js

---
+++ __tests__/index.test.js
@@ -0,0 +1,14 @@
+import supertest from 'supertest'
+import app from '../index.js'
+
+describe('POST /login', () => {
+	
+	describe('inputed a username and password', () => {
+		
+	});
+
+	describe('when the username or password is missing', () => {
+		
+	});
+
+});

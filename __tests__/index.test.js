5262dd0a110e1c7a1de69c9be559930edeadba7c
Daryush Balsara <56945036+ligmanoots@users.noreply.github.com>
2022-05-05 03:23:09 +0000

    setting up tyhe unit tests for endpoints

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,14 +1,12 @@
-import supertest from 'supertest'
-import app from '../index.js'
+import supertest from 'supertest';
+import app from '../index.js';
+import path from 'path';
+const __dirname = path.resolve();
 
-describe('POST /login', () => {
-	
-	describe('inputed a username and password', () => {
-		
-	});
-
-	describe('when the username or password is missing', () => {
-		
-	});
-
-});
+describe("GET /", () => {
+	test("it should respond with the index.html page", async () => {
+		const response = await request(app).get("/");
+		expect (response.body).toBe(path.join(__dirname + "../templates/index.html"));
+		expect(response.statusCode).toBe(200);
+	})
+})

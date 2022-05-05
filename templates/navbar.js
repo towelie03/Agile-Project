85aabcbc908b2bcdd256493fe635d5db2a39898e
ligmanoots <dbalsara2003@gmail.com>
2022-05-04 19:07:30 -0700

    turned this into js

--- templates/navbar.js
+++ templates/navbar.js
@@ -1,48 +1,50 @@
 import {LitElement, css, html} from 'lit';
-import {customElement, customElement, property} from 'lit/decorators.js';
 
-@customElement('navbar')
 export class Navbar extends LitElement {
     static styles = css `
-    
+        :host {
+            color: blue;
+        } 
     `;
 
     render() {
         return html `
-            
             <nav>
             <ul class="nav_link_list">
             <li class="nav_link">
             <a href="/" class="
             nav_link_a
             link__hover-effect
-            ">Home</a>
+            ">home</a>
             </li>
             <li class="nav_link">
             <a href="about" class="
             nav_link_a
             link__hover-effect
-            ">About</a>
+            ">about</a>
             </li>
             <li class="nav_link">
             <a href="story" class="
             nav_link_a
             link__hover-effect
-            ">Story</a>
+            ">story</a>
             </li>
             <li class="nav_link">
             <a href="timers" class="
             nav_link_a
             link__hover-effect
-            ">Timer</a>
+            ">timer</a>
             </li>
             <li class="nav_link">
             <a href="login" class="
             nav_link_a
             link__hover-effect
-            "> Login/Signup</a>
+            "> login/signup</a>
             </li>
             </nav>
-        `;
+                `;
     }
 }
+
+customElements.define('navbar', Navbar);
+

0b61a447cc5e764faf9d8fb588cc688537da3c3f
ligmanoots <dbalsara2003@gmail.com>
2022-05-04 21:36:39 -0700

    turned it into a function

--- config/db.js
+++ config/db.js
@@ -1,37 +1,9 @@
-// const mongoose = require('mongoose');
+import { MongoClient } from "mongodb";
 
 const pass = encodeURIComponent("P@ssw0rd")
-
 const con_string = `mongodb+srv://admin:${pass}@agile.ja8u2.mongodb.net/tester?retryWrites=true&w=majority`;
 
-/* const client = mongoose.connect(con_string, {useNewUrlParser: true})
-
- mongoose.connection.on("error", function(error) {
-   console.log(error)
- })
-
- mongoose.connection.on("open", function() {
-   console.log("Connected to MongoDB database.")
-})*/
-
-import { MongoClient } from "mongodb";
 const cred = [ {'admin': 'pwd'}]
-
-
 const client = new MongoClient(con_string);
-/*async function run() {
-  try {
-    await client.connect();
-    const database = client.db("tester");
-    const test = database.test;
-    // create a document to insert
-    await test.insertOne(cred);
-        console.log(`A document was inserted with the _id: ${new_cred.insertedId}`)
-;
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir)*/
 
-export default db;
+export default data_base

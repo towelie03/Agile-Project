ac947f09ba476f2992af76760155d347e4b88f5c
ligmanoots <dbalsara2003@gmail.com>
2022-05-05 15:12:00 -0700

    got the db file to work

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config()
 
 mongoose.Promise = global.Promise
 const MONGODB_URI = process.env.uri
-const db = mongoose.connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI);
 
-export default db;
+

ad6dcbe9a9c06867fb40b6d4f3f62ec4a1d68264
Ayman <aymanm979@gmail.com>
2022-05-05 11:14:32 -0700

    fixed nav and html hrefs

--- templates/src/nav.js
+++ templates/src/nav.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-
  function navs(){
-  const nav_div = document.querySelector("#navbar")
+  // const nav_div = document.querySelector("#navbar")
+  const nav_div = document.getElementById("navbar")
   nav_div.insertAdjacentHTML("beforebegin",
   `   <nav>
   <ul class="nav_link_list">
@@ -12,7 +11,7 @@
   ">Home</a>
 </li>
 <li class="nav_link">
-<a href="about.html" class="
+<a href="about" class="
   nav_link_a
   link__hover-effect
   ">About</a>
@@ -35,49 +34,9 @@
   link__hover-effect
   "> Login/Signup</a>
 </li>
+<button class="" onclick="LDmode()">Toggle dark mode</button>
 
 </nav> `
   )
 }
 navs()
-=======
-const navTemplate = document.createElement('template');
-
-navTemplate.innerHTML = `
-<nav>
-<ul class="nav_link_list">
-  <li class="nav_link">
-    <a href="index.html" class="
-        nav_link_a
-        link__hover-effect
-        ">Home</a>
-  </li>
-  <li class="nav_link">
-    <a href="about.html" class="
-        nav_link_a
-        link__hover-effect
-        ">About</a>
-  </li>
-  <li class="nav_link">
-    <a href="story.html" class="
-        nav_link_a
-        link__hover-effect
-        ">Story</a>
-  </li>
-  <li class="nav_link">
-    <a href="timers.html" class="
-        nav_link_a
-        link__hover-effect
-        ">Timer</a>
-  </li>
-  <li class="nav_link">
-    <a href="login.html" class="
-        nav_link_a
-        link__hover-effect
-        "> Login/Signup</a>
-  </li>
-
-</nav>`;
-
-document.body.appendChild(navTemplate.content)
->>>>>>> 21b9241fd00e5414e61feb962a360d787ee4653f

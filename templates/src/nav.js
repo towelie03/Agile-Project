f63df237df3b4acab6d1b5cb997604c925b3f6b2
aurilyn <caobenny122@gmail.com>
2022-05-04 17:08:49 -0700

    made js to hold template for nav bar so we dont need to change nav bar everywhere

--- templates/src/nav.js
+++ templates/src/nav.js
@@ -1,46 +1,39 @@
-import {LitElement, html} from 'lit';
+const navTemplate = document.createElement('template');
 
-class MyElement extends LitElement {
-  render() {
-    return html`
+navTemplate.innerHTML = `
+<nav>
+<ul class="nav_link_list">
+  <li class="nav_link">
+    <a href="index.html" class="
+        nav_link_a
+        link__hover-effect
+        ">Home</a>
+  </li>
+  <li class="nav_link">
+    <a href="about.html" class="
+        nav_link_a
+        link__hover-effect
+        ">About</a>
+  </li>
+  <li class="nav_link">
+    <a href="story.html" class="
+        nav_link_a
+        link__hover-effect
+        ">Story</a>
+  </li>
+  <li class="nav_link">
+    <a href="timers.html" class="
+        nav_link_a
+        link__hover-effect
+        ">Timer</a>
+  </li>
+  <li class="nav_link">
+    <a href="login.html" class="
+        nav_link_a
+        link__hover-effect
+        "> Login/Signup</a>
+  </li>
 
-      <nav>
-            <ul class="nav_link_list">
-            <li class="nav_link">
-            <a href="index.html" class="
-            nav_link_a
-            link__hover-effect
-            ">Home</a>
-      </li>
-      <li class="nav_link">
-        <a href="about.html" class="
-            nav_link_a
-            link__hover-effect
-            ">About</a>
-      </li>
-      <li class="nav_link">
-        <a href="story.html" class="
-            nav_link_a
-            link__hover-effect
-            ">Story</a>
-      </li>
-      <li class="nav_link">
-        <a href="timers.html" class="
-            nav_link_a
-            link__hover-effect
-            ">Timer</a>
-      </li>
-      <li class="nav_link">
-        <a href="login.html" class="
-            nav_link_a
-            link__hover-effect
-            "> Login/Signup</a>
-      </li>
+</nav>`;
 
-  </nav>
-
-
-          `;
-  }
-}
-customElements.define('nav_bar', navbar);
+document.body.appendChild(navTemplate.content)

02fa1d9dbe64780ba3160cfc8145b77be89fe1a0
ligmanoots <dbalsara2003@gmail.com>
2022-05-05 17:18:46 -0700

    changed the schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose
 
-const userModel = new Schema({
-    full_name: String,
-    email: String,
-    username: String,
-    password: String,
+const userSchema = new Schema({
+    full_name: { type: String, required: true },
+    email: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
   });
+
+const userModel = new mongoose.model('User', userSchema)
 
 export default userModel
 

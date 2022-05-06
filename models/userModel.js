97b6c93d53b4825487266d6fc4be480130af2771
ligmanoots <dbalsara2003@gmail.com>
2022-05-06 08:26:25 -0700

    removed the unneeded fields in the user signup and login

--- models/userModel.js
+++ models/userModel.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-    full_name: { type: String, required: true },
-    email: { type: String, required: true },
     username: { type: String, required: true },
     password: { type: String, required: true },
   });

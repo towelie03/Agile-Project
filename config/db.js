4bcc037d5ee917f234b856f4599aea3eb69d4a7b
Ayman <aymanm979@gmail.com>
2022-05-10 10:50:41 -0700

    fixed connection issues

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import app from '../index.js'
 import { config } from "dotenv";
 config({ path: process.ENV })
-import User from '../models/userModel.js';
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri,

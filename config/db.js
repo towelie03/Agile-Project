688a0fa83e2fbee9d934b30b76819dc24d97dd10
ligmanoots <dbalsara2003@gmail.com>
2022-05-11 16:59:45 -0700

    fuck this database

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import app from '../index.js'
+import app from '../index.js';
 import { config } from "dotenv";
 config({ path: process.ENV })
 
@@ -11,18 +11,17 @@ db.connect(uri,
 	}).then(() => console.log("Connected to DB Successfully")
 	).catch(err => console.log(err));
 
-export default db
-
 
 // this is for testing and use on my laptop in case the .env import dosent work
-/*const uri = ('mongodb+srv://admin:Password@agile.ja7u2.mongodb.net/users?retryWrites=true&w=majority');
+/*const uri = ('mongodb+srv://admin:Password@agile.ja8u2.mongodb.net/users?retryWrites=true&w=majority');
 db.connect(uri,
 	{
 		useNewUrlParser:true
 	}).then(() => console.log("Connected to DB Successfully")
-	).catch(err => console.log(err));
+	).catch(err => console.log(err));*/
 
-export default db*/
+
+export default db;
 
 
 

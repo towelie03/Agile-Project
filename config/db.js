7be44de7ad1219189abc9c4b3627026fef3aac0b
ligmanoots <dbalsara2003@gmail.com>
2022-05-11 16:25:02 -0700

    exported file to get ready for TDD testing

--- config/db.js
+++ config/db.js
@@ -3,12 +3,28 @@ import app from '../index.js'
 import { config } from "dotenv";
 config({ path: process.ENV })
 
+const db = mongoose
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri,
+db.connect(uri,
 	{
 		useNewUrlParser:true
 	}).then(() => console.log("Connected to DB Successfully")
-	).catch(err => console.log(err));	
+	).catch(err => console.log(err));
+
+export default db
+
+
+// this is for testing and use on my laptop in case the .env import dosent work
+/*const uri = ('mongodb+srv://admin:Password@agile.ja7u2.mongodb.net/users?retryWrites=true&w=majority');
+db.connect(uri,
+	{
+		useNewUrlParser:true
+	}).then(() => console.log("Connected to DB Successfully")
+	).catch(err => console.log(err));
+
+export default db*/
+
+
 
 
 

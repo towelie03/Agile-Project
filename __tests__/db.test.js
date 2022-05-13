91f2987dab6915180a2d6ac5b46a7a4532959de8
ligmanoots <dbalsara2003@gmail.com>
2022-05-12 22:11:57 -0700

    added some comments as to what im doing in the file

--- __tests__/db.test.js
+++ __tests__/db.test.js
@@ -4,8 +4,10 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import client from '../config/db.js';
 const uri = "mongodb+srv://admin:Password@agile.ja8u2.mongodb.net/users?retryWrites=true&w=majority";
 
+/*this generates an unique objectid for every user. it allows multiple people to have the same username*/
 const newObjectId = new ObjectId();
 
+//this tests the connection to the db
 describe('insert', () => {
   let connection;
   let db;
@@ -22,12 +24,15 @@ describe('insert', () => {
     await connection.close();
   });
 
+  // this inserts a random user into the db. 
   it('should insert a doc into collection', async () => {
     const users = db.collection('usernames');
 
-    const mockUser = {_id: newObjectId , name: 'ligmanoots'};
+    //assigns a mock user to have a objectid and a username
+    const mockUser = {_id: newObjectId , uname: 'ligmanoots'};
     await users.insertOne(mockUser);
 
+    //checks if inserted user is the same as the mock user
     const insertedUser = await users.findOne({ _id: newObjectId });
     expect(insertedUser).toEqual(mockUser);
   });

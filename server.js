2045b42508a81a6db98d7904a5d6034ffca00ea0
Daryush Balsara <56945036+ligmanoots@users.noreply.github.com>
2022-05-16 19:58:24 +0000

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-import app from './index.js'
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => {
-    console.log(`Running on port ${port}`);
-    console.log('press CTRL + C to quit');
-});

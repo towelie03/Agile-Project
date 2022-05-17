89e0b757635edb63e2061574db61712fd098b135
AndrewTake <andrew6041@gmail.com>
2022-05-17 10:27:50 -0700

    upload

--- templates/src/nav.js
+++ templates/src/nav.js
@@ -1,46 +1,61 @@
-
 function navs() {
-  // const nav_div = document.querySelector("#navbar")
-  const nav_div = document.getElementById("navbar")
-  nav_div.insertAdjacentHTML("beforebegin",
-    `   
-    <nav>
-      <ul class="nav_link_list">
-      <li class="nav_link">
-      <a href="/" class="
-      nav_link_a
-      ">Home</a>
-      </li>
-      <li class="nav_link">
-      <a href="about" class="
-        nav_link_a
-        ">About</a>
-      </li>
-      <li class="nav_link">
-      <a href="story" class="
-        nav_link_a
-        ">Story</a>
-      </li>
-      <li class="nav_link">
-      <a href="timers" class="
-        nav_link_a
-        ">Timer</a>
-      </li>
-      <li class="nav_link">
-      <a href="login" class="
-        nav_link_a 
-        ">Login/Signup</a>
-      </li>
-      <li class="nav_link">
-      <a href="custom" class="
-        nav_link_a
-        ">Custom</a>
-      </li>
+// const nav_div = document.querySelector("#navbar")
+const nav_div = document.getElementById("navbar")
+nav_div.insertAdjacentHTML("beforebegin",
+`
+<header>
+
+  <body>
+    <div class="app">
+      <div class="header-container">
+        <div class="container">
+          <div class="header-section d-flex justify-content-between align-items-center">
+            <div class="header-logo">
+              <a href="/">
+                <img src="img/logo-light.png">
+              </a>
+            </div>
+            <div class="header-menu">
+              <ul class="nav nav-light">
+                <li class="nav-item active">
+                  <a class="nav-link" href="/">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about">
+                    About
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="story">
+                    Story
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="timers">
+                    Timer
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="login">
+                    Login/Signup
+                  </a>
+                </li>
+              </ul>
+              <div class="mobile-menu">
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+          </div>
+        </div>
+</header>
 
 
-      <button class="dark-mode-toggle" onclick="LDmode()">|</button>
-    </nav>
-    `
-  )
+<button class="dark-mode-toggle" onclick="LDmode()">|</button>
+</nav>
+`
+)
 }
 navs()
